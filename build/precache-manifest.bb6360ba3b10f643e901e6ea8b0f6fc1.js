2eb22ffef9f8b95fdd6967af282c820868d124fc
Chethan Kothwal <chethankothwal@Chethans-MacBook-Pro.local>
2020-06-18 11:48:25 -0500

    Adding build

---
+++ build/precache-manifest.bb6360ba3b10f643e901e6ea8b0f6fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225c334e3d8cc35ca69b6dc60ab17940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21f30a34beefd7a40848",
+    "url": "/static/css/main.dbfc420b.chunk.css"
+  },
+  {
+    "revision": "6c18e541495590282ce8",
+    "url": "/static/js/2.5d9e640f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5d9e640f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f30a34beefd7a40848",
+    "url": "/static/js/main.cc41f3c0.chunk.js"
+  },
+  {
+    "revision": "6425563572243aa38c17",
+    "url": "/static/js/runtime-main.3faa22e5.js"
+  }
+]);
